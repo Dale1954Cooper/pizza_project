985dd8b58f65b00ebe4e6b122444a8bc83beb9cc
Dale1954Cooper <DaleCooper1954Olga@gmail.com>
2022-02-07 19:06:17 +0300

    Added a navigation bar, linked to with pages and added my own hook

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,58 @@
+import React, {FC} from 'react';
+import {Layout, Menu, Row} from "antd";
+
+import {useTypedSelector} from '../hooks/useTypedSelector';
+import {useHistory} from "react-router-dom";
+import {RoutesNames} from "../routes/routes";
+
+
+import {isAdmin} from "../store/reducers/auth/authReducer";
+
+const Navbar: FC = () => {
+    const {isAuth} = useTypedSelector(state => state.authReducer);
+    const history = useHistory()
+
+    return (
+        <Layout.Header>
+            <Row justify="end">
+                {!isAuth ?
+                    <Menu theme='dark' mode='horizontal' selectable={false}>
+                        <Menu.Item onClick={() => history.push(RoutesNames.LOGIN)}>
+                            Login
+                        </Menu.Item>
+                        <Menu.Item onClick={() => history.push(RoutesNames.REGISTRATION)}>
+                            Registration
+                        </Menu.Item>
+                        <Menu.Item onClick={() => history.push(RoutesNames.MENU)}>
+                            Menu
+                        </Menu.Item>
+                    </Menu>
+                    :
+                    !isAdmin ?
+                        <Menu theme='dark' mode='horizontal' selectable={false}>
+                            <Menu.Item onClick={() => history.push(RoutesNames.MENU)}>
+                                Menu
+                            </Menu.Item>
+                            <Menu.Item onClick={() => history.push(RoutesNames.USER_PAGE)}>
+                                User page
+                            </Menu.Item>
+                        </Menu>
+                        :
+                        <Menu theme='dark' mode='horizontal' selectable={false}>
+                            <Menu.Item onClick={() => history.push(RoutesNames.MENU)}>
+                                Menu
+                            </Menu.Item>
+                            <Menu.Item onClick={() => history.push(RoutesNames.USER_PAGE)}>
+                                User page
+                            </Menu.Item>
+                            <Menu.Item onClick={() => history.push(RoutesNames.MANAGER_PAGE)}>
+                                Manager page
+                            </Menu.Item>
+                        </Menu>
+                }
+            </Row>
+        </Layout.Header>
+    );
+};
+
+export default Navbar;
