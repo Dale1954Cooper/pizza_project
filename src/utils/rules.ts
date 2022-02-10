db64a1a8565ef29fe97daa079b8189ee196c0aaf
Dale1954Cooper <DaleCooper1954Olga@gmail.com>
2022-02-10 17:01:39 +0300

    Added id generator

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -2,5 +2,7 @@ export const rules = {
     required: (message: string = 'required field') => ({
         required: true,
         message
-    })
+    }),
+    generateId: () => 'id' + Math.random().toString(16).slice(2)
+
 }
