c95e12981d3e8b98b7344d2cf8e29e68943c61da
Dale1954Cooper <DaleCooper1954Olga@gmail.com>
2022-02-14 20:10:09 +0300

    Added firebase in project

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import firebase from "firebase/compat/app";
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDlT2FbfmvSw5D4Ngr2gU4iS3QAsBrgNOY",
+    authDomain: "pizza-delivery-18904.firebaseapp.com",
+    databaseURL: "https://pizza-delivery-18904-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "pizza-delivery-18904",
+    storageBucket: "pizza-delivery-18904.appspot.com",
+    messagingSenderId: "932715844181",
+    appId: "1:932715844181:web:73a8d5886538cc8a337adc"
+});
+
+export default firebase;
+
