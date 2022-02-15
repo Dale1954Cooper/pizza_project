2a1a7c0f96835a5e743b4a200c35afbdb1bfb532
Dale1954Cooper <DaleCooper1954Olga@gmail.com>
2022-02-15 21:48:01 +0300

    Added useEffect with some logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import {Layout} from "antd";
 
 import Navbar from "./components/Navbar";
 import AppRoutes from "./components/AppRoutes";
+import {useDispatch} from "react-redux";
+import {useTypedSelector} from "./hooks/useTypedSelector";
+import {AuthActionCreator} from "./store/reducers/auth/actionCreator";
+import firebase from './firebase';
 
 
 const App: FC = () => {
+    const dispatch = useDispatch()
+    const {isLoading} = useTypedSelector(state => state.authReducer);
+
+    useEffect(()=>{
+        dispatch(AuthActionCreator.setIsLoading(true));
+        const unsubscribe = firebase.auth().onAuthStateChanged(async(user) =>{
+            if(user){
+                await dispatch(AuthActionCreator.getUserById(user.uid));
+                if (!user.emailVerified) {
+                    dispatch(AuthActionCreator.setNeedVerification(true))
+                }
+            }
+            dispatch(AuthActionCreator.setIsLoading(false));
+        });
+
+        return () => {
+            unsubscribe();
+        }
+    }, [dispatch]);
+
     return (
         <Layout>
             <Navbar/>
