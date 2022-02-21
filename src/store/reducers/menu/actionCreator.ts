64dbb2ae078e5e00d83b97c4c773c231e9a6743c
Dale1954Cooper <DaleCooper1954Olga@gmail.com>
2022-02-21 16:59:46 +0300

    Added menu action creator

--- src/store/reducers/menu/actionCreator.ts
+++ src/store/reducers/menu/actionCreator.ts
@@ -1,6 +1,16 @@
 import {AppDispatch} from "../../store";
 import firebase from '../../../firebase'
-import {MenuActionEnum, SetMenuList} from './types'
+import {
+    MenuActionEnum,
+    SetCertainMenuList,
+    SetCurrentListName,
+    SetCurrentMenuList,
+    SetError,
+    SetMenuList
+} from './types'
+import axios from "axios";
+import {MenuItemModel} from "../../../models/menu/MenuItemModel";
+import {MenuListModel} from "../../../models/menu/MenuListModel";
 
 interface dataNames {
     names: string[];
@@ -11,24 +21,56 @@ export const MenuActionCreator = {
         type: MenuActionEnum.SET_MENU_LIST,
         payload: list
     }),
+    setMenu: (menu: MenuListModel[]): SetCertainMenuList => ({
+        type: MenuActionEnum.SET_CERTAIN_MENU_LIST,
+        payload: menu
+    }),
+    setCurrentListName: (name: string): SetCurrentListName => ({
+        type: MenuActionEnum.SET_CURRENT_LIST_NAME,
+        payload: name
+    }),
+    setCurrentMenuList: (list: MenuItemModel[]): SetCurrentMenuList => ({
+        type: MenuActionEnum.SET_CURRENT_MENU_LIST,
+        payload: list
+    }),
+    setError: (error: string): SetError => ({
+        type: MenuActionEnum.SET_ERROR,
+        payload: error
+    }),
 
     loadMenuList: () => async (dispatch: AppDispatch) => {
-    try {
-        const res = await firebase.firestore().collection('menuList').doc('list').get();
-        if (res) {
-            const data = res.data() as dataNames;
-            dispatch(MenuActionCreator.setMenuList(data.names))
+        try {
+            const res = await firebase.firestore().collection('menuList').doc('list').get();
+            if (res) {
+                const data = res.data() as dataNames;
+                dispatch(MenuActionCreator.setMenuList(data.names))
+            }
+        } catch (e) {
+            console.log("Error in menuAction in load menu list =>  ", e)
         }
-    } catch (e) {
-        console.log("Error in menuAction in load menu list =>  ", e)
-    }
-},
+    },
 
-    loadCertainMenuList:() => async (dispatch: AppDispatch) => {
-    try {
+    loadCertainMenuList: () => async (dispatch: AppDispatch) => {
+        try {
 
-    } catch (e) {
-        console.log("Error in menuAction in load certain menu list =>  ", e)
+        } catch (e) {
+            console.log("Error in menuAction in load certain menu list =>  ", e)
+        }
+    },
+
+    getMoc: (name: string) => async (dispatch: AppDispatch) => {
+        try {
+            const res = await axios.get<MenuItemModel[]>(`./${name}.json`);
+            if (res) {
+                const newMenuList: MenuListModel = {
+                    name: name,
+                    items: res.data
+                }
+                const newList: MenuListModel[] = [newMenuList];
+                dispatch(MenuActionCreator.setMenu(newList))
+            }
+        } catch (e) {
+            console.log(e);
+        }
     }
-}
 }
