485ca6a5aca171a1eff8a7d3a91c7ff30440581f
Dale1954Cooper <DaleCooper1954Olga@gmail.com>
2022-02-25 21:19:42 +0300

    Took out logic of tags and size-price complex into separate components

---
+++ src/components/SizePriceComplex.tsx
@@ -0,0 +1,49 @@
+import {Select} from 'antd';
+import React, {FC, useState} from 'react';
+import {SizePriceModel} from "../models/menu/SizePriseModel";
+import {FoodDimensionEnum} from "../models/menu/FoodDimensionEnum";
+
+const {Option} = Select
+
+interface Props {
+    sizePrice: SizePriceModel[]
+    dimension: FoodDimensionEnum
+    setSizePriseToOrder: (set: SizePriceModel) => void
+}
+
+const SizePriceComplex: FC<Props> = ({
+                                         sizePrice,
+                                         dimension,
+                                         setSizePriseToOrder
+                                     }) => {
+    const [price, setPrice] = useState(sizePrice[0].price)
+
+    const handleChange = (size: number) => {
+        const el = sizePrice.find(i => i.size === size)
+        if (el) {
+            setSizePriseToOrder(el)
+            setPrice(el.price)
+        }
+    }
+
+    return (
+        <>
+            <Select
+                defaultValue={sizePrice[0].size}
+                onChange={handleChange}
+            >
+                {sizePrice.map(item =>
+                    <Option
+                        key={item.size}
+                        value={item.size}
+                    >
+                        {item.size} {dimension}
+                    </Option>
+                )}
+            </Select>
+            <h2 style={{left: 30}}>{price}Br</h2>
+        </>
+    );
+};
+
+export default SizePriceComplex;
---
+++ src/components/Tags.tsx
@@ -0,0 +1,18 @@
+import React, {FC} from 'react';
+import {Tag} from "antd";
+
+interface Props {
+    tags: string[]
+}
+
+const Tags: FC<Props> = ({tags}) => {
+    return (
+        <>
+            {tags.map(tag =>
+                <Tag key={tag}>{tag}</Tag>
+            )}
+        </>
+    );
+};
+
+export default Tags;
