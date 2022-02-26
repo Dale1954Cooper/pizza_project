fc461a463380e1ee7fabee268ddb12ce1fae83ce
Dale1954Cooper <DaleCooper1954Olga@gmail.com>
2022-02-26 22:43:46 +0300

    Added fixed position on screen to navigation element

--- src/components/MenuSider.tsx
+++ src/components/MenuSider.tsx
@@ -1,6 +1,6 @@
 import React, {FC, useEffect, useState} from 'react';
 import {useDispatch} from 'react-redux';
-import {Menu} from 'antd';
+import {Affix, Menu} from 'antd';
 import Sider from 'antd/es/layout/Sider';
 
 import {useTypedSelector} from '../hooks/useTypedSelector';
@@ -44,22 +44,24 @@ const MenuSider: FC = () => {
 
 
     return (
-        <Sider collapsible collapsed={isCollapsed} onCollapse={() => setIsCollapsed(!isCollapsed)}>
-            <div className='logo'/>
-            <Menu theme='dark' mode="inline" defaultSelectedKeys={['0']}>
-                {menuList.map((item, index) =>
-                    <Menu.Item
-                        key={index}
-                        onClick={() => {
-                            setCurrentListData(item);
-                            setMenuName(item)
-                        }}
-                    >
-                        {item}
-                    </Menu.Item>
-                )}
-            </Menu>
-        </Sider>
+        <Affix offsetTop={64} style={{zIndex: 10}}>
+            <Sider collapsible collapsed={isCollapsed} onCollapse={() => setIsCollapsed(!isCollapsed)}>
+                <div className='logo'/>
+                <Menu theme='dark' mode="inline" defaultSelectedKeys={['0']}>
+                    {menuList.map((item, index) =>
+                        <Menu.Item
+                            key={index}
+                            onClick={() => {
+                                setCurrentListData(item);
+                                setMenuName(item)
+                            }}
+                        >
+                            {item}
+                        </Menu.Item>
+                    )}
+                </Menu>
+            </Sider>
+        </Affix>
     );
 
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, {FC, FormEvent} from 'react';
 import {useDispatch} from 'react-redux';
 import {useHistory} from 'react-router-dom';
-import {Layout, Menu} from 'antd';
+import {Affix, Layout, Menu} from 'antd';
 
 import {useTypedSelector} from '../hooks/useTypedSelector';
 import {AuthActionCreator} from '../store/reducers/auth/actionCreator';
@@ -21,57 +21,59 @@ const Navbar: FC = () => {
     }
 
     return (
-        <Layout.Header>
-            {!isAuth ?
-                <Menu theme='dark' mode='horizontal' selectable={false}>
-                    <Menu.Item key={1} onClick={() => history.push(RoutesNames.SIGN_IN)}>
-                        Sign In
-                    </Menu.Item>
-                    <Menu.Item key={2} onClick={() => history.push(RoutesNames.SIGN_UP)}>
-                        Sign Up
-                    </Menu.Item>
-                    <Menu.Item key={3} onClick={() => history.push(RoutesNames.MENU)}>
-                        Menu
-                    </Menu.Item>
-                </Menu>
-                :
-                !isAdmin ?
+        <Affix offsetTop={0}>
+            <Layout.Header>
+                {!isAuth ?
                     <Menu theme='dark' mode='horizontal' selectable={false}>
-                        <Menu.Item key={4} onClick={() => history.push(RoutesNames.MENU)}>
+                        <Menu.Item key={1} onClick={() => history.push(RoutesNames.SIGN_IN)}>
+                            Sign In
+                        </Menu.Item>
+                        <Menu.Item key={2} onClick={() => history.push(RoutesNames.SIGN_UP)}>
+                            Sign Up
+                        </Menu.Item>
+                        <Menu.Item key={3} onClick={() => history.push(RoutesNames.MENU)}>
                             Menu
-                        </Menu.Item>
-                        <Menu.Item key={5} onClick={() => history.push(RoutesNames.USER_PAGE)}>
-                            User page
-                        </Menu.Item>
-                        {orderList.length === 0 ?
-                            null
-                            :
-                            <Menu.Item key={11} onClick={() => history.push(RoutesNames.ORDER)}>
-                                Order
-                            </Menu.Item>
-                        }
-                        <Menu.Item key={6} onClick={(e) => signOutHandler(e.domEvent)}>
-                            Logout
                         </Menu.Item>
                     </Menu>
                     :
-                    <Menu theme='dark' mode='horizontal' selectable={false}>
-                        <Menu.Item key={7} onClick={() => history.push(RoutesNames.MENU)}>
-                            Menu
-                        </Menu.Item>
-                        <Menu.Item key={8} onClick={() => history.push(RoutesNames.USER_PAGE)}>
-                            User page
-                        </Menu.Item>
-                        <Menu.Item key={9} onClick={() => history.push(RoutesNames.MANAGER_PAGE)}>
-                            Manager page
-                        </Menu.Item>
-                        <Menu.Item key={10} onClick={() => signOutHandler}>
-                            Logout
-                        </Menu.Item>
-                    </Menu>
-            }
+                    !isAdmin ?
+                        <Menu theme='dark' mode='horizontal' selectable={false}>
+                            <Menu.Item key={4} onClick={() => history.push(RoutesNames.MENU)}>
+                                Menu
+                            </Menu.Item>
+                            <Menu.Item key={5} onClick={() => history.push(RoutesNames.USER_PAGE)}>
+                                User page
+                            </Menu.Item>
+                            {orderList.length === 0 ?
+                                null
+                                :
+                                <Menu.Item key={11} onClick={() => history.push(RoutesNames.ORDER)}>
+                                    Order
+                                </Menu.Item>
+                            }
+                            <Menu.Item key={6} onClick={(e) => signOutHandler(e.domEvent)}>
+                                Logout
+                            </Menu.Item>
+                        </Menu>
+                        :
+                        <Menu theme='dark' mode='horizontal' selectable={false}>
+                            <Menu.Item key={7} onClick={() => history.push(RoutesNames.MENU)}>
+                                Menu
+                            </Menu.Item>
+                            <Menu.Item key={8} onClick={() => history.push(RoutesNames.USER_PAGE)}>
+                                User page
+                            </Menu.Item>
+                            <Menu.Item key={9} onClick={() => history.push(RoutesNames.MANAGER_PAGE)}>
+                                Manager page
+                            </Menu.Item>
+                            <Menu.Item key={10} onClick={() => signOutHandler}>
+                                Logout
+                            </Menu.Item>
+                        </Menu>
+                }
 
-        </Layout.Header>
+            </Layout.Header>
+        </Affix>
     );
 };
 
