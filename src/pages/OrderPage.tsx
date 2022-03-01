d42a38476cf315c22a9e79476e527d7af2af63c0
Dale1954Cooper <DaleCooper1954Olga@gmail.com>
2022-03-02 00:33:21 +0300

    Added order page and component on it

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -1,10 +1,13 @@
 import React, {FC} from 'react';
-import {Layout} from "antd";
+import {Layout} from 'antd';
+
+import OrderList from '../components/OrderList';
+
 
 const OrderPage: FC = () => {
     return (
         <Layout>
-
+            <OrderList/>
         </Layout>
     );
 };
