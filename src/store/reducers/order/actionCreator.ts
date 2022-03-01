04d23599e6f7c57b28163f261614e3ad8da51288
Dale1954Cooper <DaleCooper1954Olga@gmail.com>
2022-03-02 00:30:47 +0300

    Added some function to calculate order price, added item to order, increment/decrement amount of one item in order, remove item.

--- src/store/reducers/order/actionCreator.ts
+++ src/store/reducers/order/actionCreator.ts
@@ -1,7 +1,7 @@
 import {
     ConfirmOrder,
     OrderActionEnum,
-    SetOrder
+    SetOrder, SetTotalPrice
 } from "./types";
 import {AppDispatch} from "../../store";
 import {MenuItemInOrderModel} from "../../../models/menu/MenuItemInOrderModel";
@@ -15,22 +15,52 @@ export const OrderActionCreator = {
     confirmOrder: (): ConfirmOrder => ({
         type: OrderActionEnum.CONFIRM_ORDER
     }),
+    setTotalPrice: (price: number): SetTotalPrice => ({
+        type: OrderActionEnum.SET_TOTAL_PRICE,
+        payload: price,
+    }),
 
     addItemToOrder: (order: MenuItemInOrderModel[], item: MenuItemInOrderModel) => (dispatch: AppDispatch) => {
-        dispatch(OrderActionCreator.setOrder([...order, item]))
+        const index: number = order.findIndex(elem =>
+            elem.name === item.name &&
+            elem.size === item.size
+        )
+        if (index >= 0) {
+            order[index].count++;
+            dispatch(OrderActionCreator.setOrder(order));
+        } else {
+            dispatch(OrderActionCreator.setOrder([...order, item]))
+        }
     },
 
     removeItemFromOrder: (order: MenuItemInOrderModel[], item: MenuItemInOrderModel) => (dispatch: AppDispatch) => {
         const index: number = order.findIndex(elem =>
             elem.name === item.name &&
-            elem.sizePrise.size === item.sizePrise.size
+            elem.size === item.size
         );
-        if (index > 0) {
-            const orderWithoutItem: MenuItemInOrderModel[] = [
-                ...order.slice(0, index), ...order.slice(index + 1, order.length)
-            ]
-            dispatch(OrderActionCreator.setOrder(orderWithoutItem));
-        }
-    }
 
+        if (index > -1) {
+            if (order[index].count > 1) {
+                order[index].count--;
+                dispatch(OrderActionCreator.setOrder(order));
+            } else {
+                order[index].count--;
+                const orderWithoutItem: MenuItemInOrderModel[] = [
+                    ...order.slice(0, index), ...order.slice(index + 1, order.length)
+                ]
+                dispatch(OrderActionCreator.setOrder(orderWithoutItem));
+            }
+        }
+    },
+
+    calculateTotalPrice: (order: MenuItemInOrderModel[]) => (dispatch: AppDispatch) => {
+        let sum = 0;
+        order.map(item => sum += item.price * item.count);
+        dispatch(OrderActionCreator.setTotalPrice(sum));
+    },
+
+    remoAllOrder: () => (dispatch: AppDispatch) => {
+        dispatch(OrderActionCreator.setTotalPrice(0))
+        dispatch(OrderActionCreator.setOrder([]))
+    }
 }
